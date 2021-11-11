3d7738b2d638459579d8409f862ede388c8c9a65
thais paris <thais.paris.mallez@gmail.com>
2021-11-11 12:03:43 +0100

    11/11 exo 2 models ok

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -3,4 +3,5 @@ class Bookmark < ApplicationRecord
   belongs_to :list
 
   validates :comment, presence: true, length: { minimum: 6 }
+  validates :movie, uniqueness: { scope: :list }
 end
--- app/models/list.rb
+++ app/models/list.rb
@@ -1,6 +1,6 @@
 class List < ApplicationRecord
-  has_many :movies, dependent: :destroy
-  has_many :bookmarks
+  has_many :bookmarks, dependent: :destroy
+  has_many :movies, through: :bookmarks
 
   validates :name, presence: true, uniqueness: true, allow_blank: false
 
